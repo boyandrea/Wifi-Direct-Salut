8cbc5dd3aa4b9695552bbcdb5a00e3414c165115
Irfan Septiadi <boyandrea@hotmail.com>
2016-05-10 11:19:59 +0700

    appp

--- java/doubled/wifidirecttest/activity/Main.java
+++ java/doubled/wifidirecttest/activity/Main.java
@@ -1,14 +1,19 @@
 package doubled.wifidirecttest.activity;
 
+import android.os.Message;
+import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.Toast;
 
+import com.bluelinelabs.logansquare.LoganSquare;
+import com.bluelinelabs.logansquare.annotation.JsonField;
+import com.bluelinelabs.logansquare.annotation.JsonObject;
 import com.peak.salut.Callbacks.SalutCallback;
 import com.peak.salut.Callbacks.SalutDataCallback;
 import com.peak.salut.Callbacks.SalutDeviceCallback;
@@ -17,44 +22,46 @@ import com.peak.salut.SalutDataReceiver;
 import com.peak.salut.SalutDevice;
 import com.peak.salut.SalutServiceData;
 
+import java.util.ArrayList;
+
 import doubled.wifidirecttest.R;
 
-/**
- * Created by Irfan Septiadi Putra on 10/04/2016.
- */
-public class Main extends AppCompatActivity implements SalutDataCallback, View.OnClickListener {
 
-    public Button hostingBtn,startBtn;
-    public Button discoverBtn;
-    public EditText namaHost;
-    public static final String TAG = "SalutTestAppp";
+public class Main extends AppCompatActivity implements SalutDataCallback, View.OnClickListener{
+
+    /*
+        This simple activity demonstrates how to use the Salut library from a host and client perspective.
+     */
+
+    public static final String TAG = "SalutTestApp";
     public SalutDataReceiver dataReceiver;
     public SalutServiceData serviceData;
     public Salut network;
-    SalutDataCallback callback;
+    public Button hostingBtn;
+    public Button discoverBtn;
+    public Button startBtn;
+    public boolean isHost = false;
+    public boolean isClient = false;
 
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_layout);
 
         hostingBtn = (Button) findViewById(R.id.btnHost);
         discoverBtn = (Button) findViewById(R.id.btnDiscover);
         startBtn = (Button) findViewById(R.id.btnStart);
-        namaHost = (EditText) findViewById(R.id.namaHost);
-        discoverBtn.setOnClickListener(this);
+
         hostingBtn.setOnClickListener(this);
+        discoverBtn.setOnClickListener(this);
         startBtn.setOnClickListener(this);
 
-    }
 
-    private void initialize(){
         /*Create a data receiver object that will bind the callback
         with some instantiated object from our app. */
         dataReceiver = new SalutDataReceiver(this, this);
-
         /*Populate the details for our awesome service. */
-        serviceData = new SalutServiceData("testAwesomeService", 60606,namaHost.getText().toString());
+        serviceData = new SalutServiceData("testAwesomeService", 60606, "HOST");
 
         /*Create an instance of the Salut class, with all of the necessary data from before.
         * We'll also provide a callback just in case a device doesn't support WiFi Direct, which
@@ -69,73 +76,76 @@ public class Main extends AppCompatActivity implements SalutDataCallback, View.O
         });
     }
 
-    private void setupNetwork() {
-        if(!network.isRunningAsHost)
-        {
-            network.startNetworkService(new SalutDeviceCallback() {
-                @Override
-                public void call(SalutDevice salutDevice) {
-                    Toast.makeText(getApplicationContext(), "Device: " + salutDevice.instanceName + " connected.", Toast.LENGTH_SHORT).show();
-                }
-            });
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.btnDiscover:
+                    if(!isHost && isClient == false){
+                        setAsClient();
+                        isClient = true;
+                        discoverBtn.setText("Stop Client");
+                    }else if(isClient){
+                        isClient = false;
+                        network.stopServiceDiscovery(true);
+                        discoverBtn.setText("Start Client");
+                    }
 
-            hostingBtn.setText("Stop Service");
-            discoverBtn.setAlpha(0.5f);
-            discoverBtn.setClickable(false);
-        }
-        else
-        {
-            network.stopNetworkService(false);
-            hostingBtn.setText("Start Service");
-            discoverBtn.setAlpha(1f);
-            discoverBtn.setClickable(true);
+                break;
+            case R.id.btnHost:
+                if(!isClient && isHost == false){
+                    setAsHost();
+                    isHost = true;
+                    hostingBtn.setText("Stop Client");
+                }else if(isHost){
+                    isHost = false;
+                    network.stopNetworkService(true);
+                    hostingBtn.setText("Start Client");
+                }
+                break;
+            case R.id.btnStart:
+                getList();
+                break;
+            default:
+                break;
         }
     }
 
-    private void discoverServices()
-    {
-        if(!network.isRunningAsHost && !network.isDiscovering)
-        {
-            network.discoverNetworkServices(new SalutCallback() {
-                @Override
-                public void call() {
-                    Toast.makeText(getApplicationContext(), "Device: " + network.foundDevices.get(0).instanceName + " found.", Toast.LENGTH_SHORT).show();
-                }
-            }, true);
-            discoverBtn.setText("Stop Discovery");
-            hostingBtn.setAlpha(0.5f);
-            hostingBtn.setClickable(false);
-        }
-        else
-        {
-            network.stopServiceDiscovery(true);
-            discoverBtn.setText("Discover Services");
-            hostingBtn.setAlpha(1f);
-            hostingBtn.setClickable(false);
-        }
+    private void setAsHost(){
+        network.startNetworkService(new SalutDeviceCallback() {
+            @Override
+            public void call(SalutDevice salutDevice) {
+                Log.w(TAG, salutDevice.readableName + " has connected!");
+                getList();
+            }
+        });
     }
+
+    private void setAsClient(){
+        network.discoverWithTimeout(
+                new SalutCallback() {
+                    @Override
+                    public void call() {
+                        Log.w(TAG, "Look at all these devices! " + network.foundDevices.toString());
+                        getList();
+                    }
+                }, new SalutCallback() {
+                    @Override
+                    public void call() {
+                        Log.w(TAG, "Bummer, we didn't find anyone. ");
+                    }
+                },10*1000);
+    }
+
+
     @Override
     public void onDataReceived(Object o) {
 
     }
 
-    @Override
-    public void onClick(View v) {
-        if(!Salut.isWiFiEnabled(getApplicationContext()))
-        {
-            Toast.makeText(getApplicationContext(), "Please enable WiFi first.", Toast.LENGTH_SHORT).show();
-            return;
-        }
-
-        if(v.getId() == R.id.btnHost)
-        {
-            setupNetwork();
-        }
-        else if(v.getId() == R.id.btnDiscover)
-        {
-            discoverServices();
-        }else if(v.getId() == R.id.btnStart){
-            initialize();
+    public void getList(){
+        if(network.isConnectedToAnotherDevice){
+            Log.w("Device",""+network.foundDevices.size());
         }
     }
 }
+
